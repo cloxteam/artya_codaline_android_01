bb24aacd386e2a714f3a8066bbebf658ab35e06f
Artem Merezhko <cloxteam@gmail.com>
2016-02-06 18:40:33 +0200

    Creating 2 activities, but Toast is absent.

---
+++ app/src/main/java/com/artya/sample/MainActivity.java
@@ -0,0 +1,33 @@
+package com.artya.sample;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText sendText1;
+    private TextView txtFromSecond;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sendText1 = (EditText) findViewById(R.id.send_text_1);
+        txtFromSecond = (TextView) findViewById(R.id.txtFromSecond);
+
+        txtFromSecond.setText(getIntent().getStringExtra("sendText2"));
+    }
+
+    public void toSecondActivity(View view){
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra("sendText1", sendText1.getText().toString());
+        startActivity(intent);
+    }
+
+
+}
---
+++ app/src/main/java/com/artya/sample/SecondActivity.java
@@ -0,0 +1,34 @@
+package com.artya.sample;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * Created by artya on 05.02.16.
+ */
+public class SecondActivity extends AppCompatActivity {
+
+    private EditText sendText2;
+    private TextView txtFromFirst;
+    String str = "Message from second activiti is: ";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        sendText2 = (EditText) findViewById(R.id.send_text_2);
+        txtFromFirst = (TextView) findViewById(R.id.txtFromFirst);
+
+        txtFromFirst.setText(getIntent().getStringExtra("sendText1"));
+    }
+    public void toPreviousActivity(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("sendText2",str + sendText2.getText().toString());
+        startActivity(intent);
+    }
+}
